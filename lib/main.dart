60a8d179e5533100f50bebc967f9adb32eb674c3
besta29khwa <62501102+besta29khwa@users.noreply.github.com>
2022-04-22 17:46:07 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:realtime_vehicle_tracking/Login_page.dart';
-import 'package:realtime_vehicle_tracking/lib/phone-track.dart';
 import 'package:realtime_vehicle_tracking/signup_page.dart';
 import 'map_page.dart';
 import 'options_page.dart';
@@ -37,7 +36,6 @@ class MyApp extends StatelessWidget {
         '/options_page': (context)=> OptionPage(),
         // '/home_page' : (context) => HomePage(),
         '/Login_page' : (context) => LoginPage(),
-        '/lib/phone-track' : (context) => Mobile(),
         '/signup_page': (context) => SignUpPage(),
 
       },
